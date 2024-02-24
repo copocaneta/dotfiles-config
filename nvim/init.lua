d2a14c4edabbcd12ea4cfa29194d9fad8d97b14f
Thiago Bernardi <copocaneta@gmail.com>
2024-02-23 23:34:41 -0300

    refactor: adding vscode nvim plugin support

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,8 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+--  bootstrap lazy.nvim, LazyVim and your plugins
+ if vim.g.vscode then
+ 	-- VSCode extension
+ 	require("config.vscodesettings")
+ else
+    require("config.lazy")
+ end
+
